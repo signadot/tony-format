08fa75076c3f0d68fafc30dfe889b13ddcbaf14f
Scott Cotton <scott@mindowl.com>
2025-12-20 11:10:41 +0100

    import: update from directory

--- discussion/files/docs/sketchy/editors/tree-sitter-tony/bindings/rust/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .file("parser.c")
-        .include(".")
-        .compile("tree-sitter-tony");
-}
--- discussion/files/docs/sketchy/pygments-tony/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Pygments lexer for Tony syntax highlighting.
-"""
-
-from pygments_tony import TonyLexer
-
-__all__ = ['TonyLexer']
